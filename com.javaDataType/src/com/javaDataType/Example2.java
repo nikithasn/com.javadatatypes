03023433945ec933d106035bc3df38e88d44f815
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-07 12:53:55 +0530

    java data types added to

---
+++ com.javaDataType/src/com/javaDataType/Example2.java
@@ -0,0 +1,24 @@
+package com.javaDataType;
+
+public class Example2
+{
+
+	public static void main(String[] args) 
+	{
+		
+		//int ages=29,30,40;
+		//Sysstem.out.println(ages);
+		
+		int ages[]= {29,30,40};
+		
+		//System.out.println(ages[]);
+		
+		for(int age: ages)
+			
+		{
+			System.out.println(age);
+		}
+
+	}
+
+}

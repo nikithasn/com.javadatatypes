a127707fcc9f75a6a653867957859108982c98c6
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-07 11:42:10 +0530

    datatypes

---
+++ com.javaDataType/src/com/javaDataType/Example1.java
@@ -0,0 +1,48 @@
+package com.javaDataType;
+
+public class Example1 
+{
+
+	public static void main(String[] args)
+	{
+		int studentId;
+		String studentName;
+		int studentAge;
+		int studentMarks;
+		char studentGrade;
+		float studentResult;
+		String studentAddress;
+		
+		studentId = 2728;
+		studentName = "Nikitha";
+		studentAge = 21;
+		studentMarks = 931;
+		studentGrade = 'A';
+		studentResult = 9.33f;
+		studentAddress ="India";
+		
+		System.out.print("Student Id: ");
+		System.out.println(studentId);
+  
+		System.out.print("Student Name: ");
+		System.out.println(studentName);
+		
+		System.out.print("Student Age: ");
+		System.out.println(studentAge);
+		
+		System.out.print("Student Marks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("Student Grade: ");
+		System.out.println(studentGrade);
+		
+		System.out.print("Student Result: ");
+		System.out.println(studentResult);
+		
+		System.out.print("Student Address: ");
+		System.out.println(studentAddress);
+		
+
+	}
+
+}
